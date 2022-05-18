2b12e5deb77e3e6e8f636aa84e934cf563946f3b
AlexeyLipavskiy <90194900+AlexeyLipavskiy@users.noreply.github.com>
2022-05-19 01:50:43 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib_inline
+import numpy as np
+# %matplotlib inline
+x = np.ndarray(5)
+plt.plot(x**2)
+#
+matplotlib.use('GTK3Agg')
+# plt.show(block=True)
+# plt.interactive(False)
+# plt.show()
+
+#%%
